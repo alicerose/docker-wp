ef68743b97c087b44f45b54cfdec4498a5a4d9f7
Alice Rose <czk@seeds-9.com>
2020-09-02 17:23:47 +0900

    add develop environment

--- bin/install.sh
+++ bin/install.sh
@@ -9,7 +9,7 @@ chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
 wp plugin install \
-    classic-editor advanced-custom-fields wp-multibyte-patch \
+    classic-editor advanced-custom-fields wp-multibyte-patch application-passwords acf-to-rest-api wp-add-mime-types  \
     --activate --path=${WPINSTALLDIR} --allow-root
 
 wp theme activate my-theme --path=${WPINSTALLDIR} --allow-root
