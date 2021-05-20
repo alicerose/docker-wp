74928c85aaff8085f37d9c75f41a563857b1a266
Alice Rose <czk@seeds-9.com>
2021-05-20 18:01:09 +0900

    add admin related functions

--- app/themes/my-theme/functions/admin.php
+++ app/themes/my-theme/functions/admin.php
@@ -19,6 +19,21 @@
      */
     add_filter( 'admin_footer_text', 'replaceAdminFooterText' );
 
+    /**
+     * 不要な項目削除
+     */
+    add_action('admin_menu', 'removeUnnecessaryMenus');
+
+    /**
+     * 投稿画面の不要項目削除
+     */
+    add_action( 'admin_menu' , 'removeUnnecessaryMeta_Post' );
+
+    /**
+     * 固定ページの不要項目削除
+     */
+    add_action( 'admin_menu' , 'removeUnnecessaryMeta_Page' );
+
     /*-----------------------------------------------------------------------------------*/
     /* 関数 */
     /*-----------------------------------------------------------------------------------*/
@@ -44,4 +59,36 @@
      */
     function replaceAdminFooterText() {
         echo 'Footer Text';
+    }
+
+    /**
+     *
+     */
+    function removeUnnecessaryMenus () {
+        global $menu;
+//         remove_menu_page( 'edit-comments.php' );// コメント
+    }
+
+    /**
+     *
+     */
+    function removeUnnecessaryMeta_Post() {
+        if ( !current_user_can( 'administrator' ) ) {
+//            remove_meta_box( 'commentsdiv' , 'post' , 'normal' );      // コメント
+            remove_meta_box( 'tagsdiv-post_tag' , 'page' , 'side' );   // タグ
+            remove_meta_box( 'trackbacksdiv' , 'post' , 'normal' );    // トラックバック
+            remove_meta_box( 'commentstatusdiv' , 'post' , 'normal' ); // ディスカッション
+        }
+    }
+
+    /**
+     *
+     */
+    function removeUnnecessaryMeta_Page() {
+        if ( !current_user_can( 'administrator' ) ) {
+            remove_meta_box( 'commentsdiv' , 'page' , 'normal' );      // コメント
+            remove_meta_box( 'tagsdiv-post_tag' , 'page' , 'side' );   // タグ
+            remove_meta_box( 'trackbacksdiv' , 'page' , 'normal' );    // トラックバック
+            remove_meta_box( 'commentstatusdiv' , 'page' , 'normal' ); // ディスカッション
+        }
     }
