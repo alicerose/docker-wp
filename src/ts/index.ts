f80bdd4910e6d78e154bd26a5a840a9b1c364de0
Alice Rose <czk@seeds-9.com>
2022-02-10 18:31:04 +0900

    add source directory

---
+++ src/ts/index.ts
@@ -0,0 +1,19 @@
+import '@/scss/common.scss';
+import { Utilities } from './utilities';
+// import 'core-js';
+// import SampleApiClass from './models/SampleApiClass';
+
+Utilities.init();
+//
+// const sampleApi = new SampleApiClass(process.env.API_HOST ?? '', '/posts');
+// sampleApi
+//   .getPosts()
+//   .then((res) => {
+//     console.log(res);
+//     console.log(res.data);
+//     console.log(res.code);
+//     console.log(res.success);
+//   })
+//   .catch(() => {
+//     console.log('Error');
+//   });
